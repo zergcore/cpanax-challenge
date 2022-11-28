935a25f91d585eeb33cf29a825ca52c4b0e34f84
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-28 00:09:04 -0400

    Products fully working

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,55 @@
+import React, {useState} from 'react';
+import '../styles/ProductItem.scss'
+import Lightbox from "react-18-image-lightbox";
+import "react-18-image-lightbox/style.css";
+
+const ProductItem = ({ product }) => {
+
+	const [isOpen, setIsOpen] = useState(false);
+  	const [photoIndex, setPhotoIndex] = useState(0);
+	const {images}=product
+
+	return (
+	
+		<div className="ProductItem">
+			<button 
+					type="button" 
+					className="btn btn-transparent"
+					onClick={() => setIsOpen(true)}>
+				<img src={product.images[0]} alt={product.title} />
+				<div className="product-info">
+					<div>
+						<p>${product.price}</p>
+						<p>{product.title}</p>
+					</div>
+				</div>
+			</button>
+
+		{/* <ProductModal product={product}/> */}
+
+		{!!isOpen && (
+			<Lightbox
+			mainSrc={images[photoIndex]}
+			nextSrc={images[(photoIndex + 1) % images.length]}
+			prevSrc={
+				images[
+				(photoIndex + images.length - 1) % images.length
+				]
+			}
+			onCloseRequest={() => setIsOpen(false)}
+			onMovePrevRequest={() =>
+				setPhotoIndex(
+				(photoIndex + images.length - 1) % images.length
+				)
+			}
+			onMoveNextRequest={() =>
+				setPhotoIndex((photoIndex + 1) % images.length)
+			}
+			/>
+		)}
+
+		</div>
+	);
+}
+
+export default ProductItem;
--- src/containers/Products.jsx
+++ src/containers/Products.jsx
@@ -1,8 +1,74 @@
-import React from 'react'
+import React, {useState} from 'react'
+import ProductItem from '../components/ProductItem';
+import useGetProducts from '../hooks/useGetProducts';
+import '../styles/Products.scss';
+
+const API='https://dummyjson.com/products';
 
 function Products() {
+
+  const [elements, setElements] = useState(4)
+
+	const handleChange=(choice)=>{
+			setElements(choice.target.value)
+	}
+
+	const products = useGetProducts(API);
+
   return (
-    <div>Products</div>
+    
+    <section className="container-fluid my-2">
+			<div className="container">
+			  <div className="form-floating">
+					<select 
+							className="form-select" 
+							name='selectElements' 
+							id="selectElements"
+							defaultValue={4} 
+							onChange={choice => handleChange(choice)}
+					>
+						<option value="2">2</option>
+						<option value="3">3</option>
+						<option value="4">4</option>
+						<option value="6">6</option>
+					</select>
+					<label htmlFor="floatingSelect">Cuantos elementos te gustaria tener por fila?</label>
+				</div>
+			</div>
+				
+			<div className="container my-2">
+				
+				<div className="row justify-content-evenly">
+					{products.map((product, i)=>(
+						<>
+						{elements==6 && 
+							<div className="col-2 justify-content-center align-self-center" key={`${elements}${i}${product.id}`}>
+								<ProductItem product={product} key={`${elements}${product.id}`} />
+							</div>
+							}
+						{elements==4 && 
+							<div className="col-3 justify-content-center align-self-center" key={`${elements}${i}${product.id}`}>
+								<ProductItem product={product} key={`${elements}${product.id}`} />
+							</div>
+							}
+						{elements==3 && 
+							<div className="col-4 justify-content-center align-self-center" key={`${elements}${i}${product.id}`}>
+								<ProductItem product={product} key={`${elements}${product.id}`} />
+							</div>
+
+							}
+						{elements==2 && 
+							<div className="col-6 justify-content-center align-self-center" key={`${elements}${i}${product.id}`}>
+								<ProductItem product={product} key={`${elements}${product.id}`} />
+							</div>
+							}
+						</>
+					))}
+				</div>
+			</div>
+		</section>
+
+
   )
 }
 
