64639cca05b5eb86bc83469cea3db555c841a252
Zaidibeth Ramos <38891128+zergcore@users.noreply.github.com>
2022-11-27 21:32:18 -0400

    Already showing responsive appbar

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import App from './routes/App';
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App tab="home" />);
